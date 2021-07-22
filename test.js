034aaeb2858ba4566b0255acd9ddfaa234a0598c
BuDy X <85840062+MrChaby@users.noreply.github.com>
2021-07-22 10:46:37 +0530

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ Asena.addCommand({pattern: 'ravana', fromMe: false,dontAddCommandList: true }, (
 
   var image = await axios.get ('Config.MENU_LOGO', {responseType: 'arraybuffer'})
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption:*╔═▣═══❖⦁⦁⦁💢⦁⦁⦁❖═══▣═╗*\n*RAVANA BOT MENU*\n'+Config.MENUTEXT+'\n*╚═══❖═══▣ ▣════❖═══╝*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption:*`╔═▣═══❖⦁⦁⦁💢⦁⦁⦁❖═══▣═╗*\n*RAVANA BOT MENU*\n'+Config.MENUTEXT+'\n*╚═══❖═══▣ ▣════❖═══╝*
 
 
 ※‗‗‗‗‗‗‗*Media commands*‗‗‗‗‗‗‗‗※
@@ -134,6 +134,8 @@ Asena.addCommand({pattern: 'ravana', fromMe: false,dontAddCommandList: true }, (
 
 
 *╔═▣═══❖⦁⦁⦁💢⦁⦁⦁❖═══▣═╗*\n*RAVANA MENU*\n'+Config.MENUTEXT+'\n*╚═══❖═══▣ ▣════❖═══╝*
-🟦🟨🟩🟦🟨🟩🟦🟨🟩🟦🟨🟩`}) 
+🟦🟨🟩🟦🟨🟩🟦🟨🟩🟦🟨🟩
+                              
+`}) 
 
 }));
